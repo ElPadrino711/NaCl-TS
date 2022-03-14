edc40ccc788c4652cf44a7119fd5f84164743ef0
ElPadrino717 <elpadrino711@outlook.com>
2022-03-14 00:38:51 -0600

    Delete dist directory

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: process.env.token,
-    devs: ['728828371383091200', '847818806906847252'],
-    bot: { intents: ['GUILDS', 'GUILD_MESSAGES'] }
-};
